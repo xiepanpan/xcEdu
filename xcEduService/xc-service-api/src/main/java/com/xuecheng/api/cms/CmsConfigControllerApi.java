ca26075bec810057b30f8f31200082a6f91b2dc8
xiepanpan <xiepanpan@thunisoft.com>
2020-03-18 13:03:53 +0800

    :sparkles: 页面可配置轮播图

---
+++ xcEduService/xc-service-api/src/main/java/com/xuecheng/api/cms/CmsConfigControllerApi.java
@@ -0,0 +1,16 @@
+package com.xuecheng.api.cms;
+
+import com.xuecheng.framework.domain.cms.CmsConfig;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+/**
+ *  @author: xiepanpan
+ *  @Date: 2020/3/16
+ *  @Description:  cms配置控制层
+ */
+@Api(value="cms配置管理接口",description = "cms配置管理接口，提供数据模型的管理、查询接口")
+public interface CmsConfigControllerApi {
+    @ApiOperation("根据id查询CMS配置信息")
+    public CmsConfig getmodel(String id);
+}
