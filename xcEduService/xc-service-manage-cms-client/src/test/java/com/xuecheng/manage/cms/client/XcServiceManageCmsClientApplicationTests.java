a58c79c833906011c49577d883ff18bd3d3c464a
xiepanpan <xiepanpan@thunisoft.com>
2020-03-24 12:32:36 +0800

    :sparkles: 页面发布

---
+++ xcEduService/xc-service-manage-cms-client/src/test/java/com/xuecheng/manage/cms/client/XcServiceManageCmsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuecheng.manage.cms.client;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XcServiceManageCmsClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
