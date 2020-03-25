d75e5f2caf32acc89df675427e0b836a4eee8542
xiepanpan <xiepanpan@thunisoft.com>
2020-03-25 15:44:00 +0800

    :sparkles: 文件上传

---
+++ xcEduService/xc-service-base-filesystem/src/main/java/com/xuecheng/filesystem/XcServiceBaseFilesystemApplication.java
@@ -0,0 +1,13 @@
+package com.xuecheng.filesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XcServiceBaseFilesystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XcServiceBaseFilesystemApplication.class, args);
+    }
+
+}
