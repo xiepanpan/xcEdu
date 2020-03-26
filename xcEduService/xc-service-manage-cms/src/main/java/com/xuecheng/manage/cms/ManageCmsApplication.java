f363c93d77470b095b0e1dad2be75af44a1b896c
xiepanpan <xiepanpan@thunisoft.com>
2020-03-26 15:06:56 +0800

    :sparkles: 服务加入eureka管理

--- xcEduService/xc-service-manage-cms/src/main/java/com/xuecheng/manage/cms/ManageCmsApplication.java
+++ xcEduService/xc-service-manage-cms/src/main/java/com/xuecheng/manage/cms/ManageCmsApplication.java
@@ -3,11 +3,13 @@ package com.xuecheng.manage.cms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @EntityScan("com.xuecheng.framework.domain.cms")
 @ComponentScan(basePackages = {"com.xuecheng.api"})
