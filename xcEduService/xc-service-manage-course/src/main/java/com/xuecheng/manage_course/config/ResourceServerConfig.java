c0cd4f750d14b1b27cbfa5d5327531c3cda8f5dc
xiepanpan <xiepanpan@thunisoft.com>
2020-05-26 20:03:14 +0800

    :sparkles: 学习课程添加

---
+++ xcEduService/xc-service-manage-course/src/main/java/com/xuecheng/manage_course/config/ResourceServerConfig.java
@@ -0,0 +1,88 @@
+package com.xuecheng.manage_course.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
+/**
+ * @author: xiepanpan
+ * @Date: 2020/5/26
+ * @Description: 管理服务上配置授权控制
+ */
+@Configuration
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true,securedEnabled = true)//激活方法上的PreAuthorize注解
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+
+    private static final String PUBLIC_KEY ="publicKey.txt";
+
+    /**
+     * 定义JwtTokenStore  使用jwt令牌
+     * @param jwtAccessTokenConverter
+     * @return
+     */
+    @Bean
+    public TokenStore tokenStore(JwtAccessTokenConverter jwtAccessTokenConverter) {
+        return new JwtTokenStore(jwtAccessTokenConverter);
+    }
+
+    /**
+     * 定义JwtAccessTokenConverter 使用jwt令牌
+     * @return
+     */
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setVerifierKey(getPubKey());
+        return converter;
+    }
+
+    /**
+     * 获取非对称加密的公钥
+     * @return
+     */
+    private String getPubKey() {
+        Resource resource = new ClassPathResource(PUBLIC_KEY);
+        InputStreamReader inputStreamReader = null;
+        try {
+            inputStreamReader = new InputStreamReader(resource.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            return bufferedReader.lines().collect(Collectors.joining("\n"));
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    /**
+     * Http安全配置 对每个到达的系统的http请求链接进行校验
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        //所有请求必须认证通过
+        http.authorizeRequests()
+                //以下路径放行
+                .antMatchers("/v2/api-docs", "/swagger-resources/configuration/ui",
+                        "/swagger-resources","/swagger-resources/configuration/security",
+                        "/swagger-ui.html","/webjars/**","/course/coursepic/list/**","/course/courseview/**")
+                .permitAll()
+                .anyRequest().authenticated();
+    }
+}
+
--- xcEduService/xc-service-manage-course/src/main/java/com/xuecheng/manage_course/controller/CourseController.java
+++ xcEduService/xc-service-manage-course/src/main/java/com/xuecheng/manage_course/controller/CourseController.java
@@ -4,12 +4,18 @@ import com.xuecheng.api.course.CourseControllerApi;
 import com.xuecheng.framework.domain.course.CoursePic;
 import com.xuecheng.framework.domain.course.Teachplan;
 import com.xuecheng.framework.domain.course.TeachplanMedia;
+import com.xuecheng.framework.domain.course.ext.CourseInfo;
 import com.xuecheng.framework.domain.course.ext.CourseView;
 import com.xuecheng.framework.domain.course.ext.TeachplanNode;
+import com.xuecheng.framework.domain.course.request.CourseListRequest;
 import com.xuecheng.framework.domain.course.response.CoursePublishResult;
+import com.xuecheng.framework.model.response.QueryResponseResult;
 import com.xuecheng.framework.model.response.ResponseResult;
+import com.xuecheng.framework.utils.XcOauth2Util;
+import com.xuecheng.framework.web.BaseController;
 import com.xuecheng.manage_course.service.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -19,17 +25,19 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 @RequestMapping("/course")
-public class CourseController implements CourseControllerApi {
+public class CourseController extends BaseController implements CourseControllerApi {
 
     @Autowired
     CourseService courseService;
 
     @Override
+    @PreAuthorize("hasAuthority('course_teachplan_list')")
     @GetMapping("/teachplan/list/{courseId}")
     public TeachplanNode findTeachplanList(@PathVariable("courseId") String courseId) {
         return courseService.findTeachplanList(courseId);
     }
 
+    @PreAuthorize("hasAuthority('course_teachplan_add')")
     @Override
     @PostMapping("/teachplan/add")
     public ResponseResult addTeachplan(@RequestBody Teachplan teachplan) {
@@ -42,6 +50,8 @@ public class CourseController implements CourseControllerApi {
         return courseService.addCoursePic(courseId,pic);
     }
 
+    //当用户拥有course_pic_list权限时候方可访问此方法
+    @PreAuthorize("hasAuthority('course_pic_list')")
     @Override
     @GetMapping("/coursepic/list/{courseId}")
     public CoursePic findCoursePic(@PathVariable("courseId") String courseId) {
@@ -76,5 +86,17 @@ public class CourseController implements CourseControllerApi {
     @PostMapping("/savemedia")
     public ResponseResult savemedia(@RequestBody TeachplanMedia teachplanMedia) {
         return courseService.savemedia(teachplanMedia);
+    }
+
+    @Override
+    @GetMapping("/coursebase/list/{page}/{size}")
+    public QueryResponseResult<CourseInfo> findCourseList(@PathVariable("page") int page,
+                                                          @PathVariable("size") int size,
+                                                          CourseListRequest courseListRequest) {
+        XcOauth2Util xcOauth2Util = new XcOauth2Util();
+        XcOauth2Util.UserJwt userJwt = xcOauth2Util.getUserJwtFromHeader(request);
+        String companyId = userJwt.getCompanyId();
+        QueryResponseResult<CourseInfo> queryResponseResult = courseService.findCourseList(companyId, page, size, courseListRequest);
+        return queryResponseResult;
     }
 }
